1110b18f72517341a82f6659e2011d4d5095cefd
kbbushman <k.bushman@yahoo.com>
2021-06-08 20:20:54 -0700

    Updates/adds error handlers

--- seed.js
+++ seed.js
@@ -1,12 +1,25 @@
 const db = require('./models');
 const data = require('./gameData.json');
 
-db.Game.deleteMany({}, (err, deletedGames) => {
-    db.Game.create(data.games, (err, seededGames) => {
-        if (err) console.log(err);
-        
-        console.log(data.games.length, 'games created successfully');
-        
-        process.exit();
-    });
+// Delete
+db.Game.deleteMany({}, (err, result) => {
+  if (err) {
+    console.log(err);
+    process.exit();
+  }
+  
+  console.log(result.deletedCount,'games deleted');
+
+  // Create
+  db.Game.create(data.games, (err, seededGames) => {
+    if (err) {
+      console.log(err);
+      process.exit();
+    }
+    
+    console.log(seededGames.length, 'games created successfully');
+    console.log('done!');
+
+    process.exit();
+  });
 });
