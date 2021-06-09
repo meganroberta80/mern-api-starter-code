a1b131158ca9aef10be87c0be234a9aceece51b1
kbbushman <k.bushman@yahoo.com>
2021-06-08 19:56:21 -0700

    Updates filenames and imports

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,3 @@
 module.exports = {
-    games: require('./games'),
+  games: require('./gamesController'),
 };
--- models/index.js
+++ models/index.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.MONGODB_URI || "mongodb://localhost:27017/gamelib";
+const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/gamelib';
+
 const configOptions = {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
 };
 
 mongoose.connect(connectionString, configOptions)
-    .then(() => console.log('MongoDB successfully connected...'))
-    .catch(err => console.log(`MongoDB connection error: ${err}`));
+  .then(() => console.log('MongoDB successfully connected...'))
+  .catch((err) => console.log(`MongoDB connection error: ${err}`));
 
 module.exports = {
-    Game: require('./game'),
+  Game: require('./Game'),
 };
