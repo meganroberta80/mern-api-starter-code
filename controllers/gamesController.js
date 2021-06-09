ec9c1311762f7239e0574e4cc45cc8d14d1b7314
kbbushman <k.bushman@yahoo.com>
2021-06-08 20:17:52 -0700

    Updates error handlers

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -1,50 +1,70 @@
 const db = require('../models');
 
 const index = (req, res) => {
-    db.Game.find({}, (err, foundGames) => {
-        if (err) console.log('Error in games#index:', err);
+  db.Game.find({}, (err, foundGames) => {
+    if (err) {
+      console.log('Error in games#index:', err);
 
-        res.send("Incomplete games#index controller function");
-    });
+      res.send("Incomplete games#index controller function");
+    }
+
+    res.send("Incomplete games#index controller function");
+  });
 };
 
 const show = (req, res) => {
-    db.Game.findById(req.params.id, (err, foundGame) => {
-        if (err) console.log('Error in games#show:', err);
+  db.Game.findById(req.params.id, (err, foundGame) => {
+    if (err) {
+      console.log('Error in games#show:', err);
 
-        res.send("Incomplete games#show controller function");
-    });
+      res.send("Incomplete games#show controller function");
+    }
+
+    res.send("Incomplete games#show controller function");
+  });
 };
 
 const create = (req, res) => {
-    db.Game.create(req.body, (err, savedGame) => {
-        if (err) console.log('Error in games#create:', err);
+  db.Game.create(req.body, (err, savedGame) => {
+    if (err) {
+      console.log('Error in games#create:', err);
 
-        res.send("Incomplete games#create controller function");
-    });
+      res.send("Incomplete games#create controller function");
+    }
+
+    res.send("Incomplete games#create controller function");
+  });
 };
 
 const update = (req, res) => {
-    db.Game.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedGame) => {
-        if (err) console.log('Error in games#update:', err);
+  db.Game.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedGame) => {
+    if (err) {
+      console.log('Error in games#update:', err)
 
-        res.send("Incomplete games#update controller function");
-    });
+      res.send("Incomplete games#update controller function");
+    }
+
+    res.send("Incomplete games#update controller function");
+  });
 };
 
 const destroy = (req, res) => {
-    db.Game.findByIdAndDelete(req.params.id, (err, deletedGame) => {
-        if (err) console.log('Error in games#destroy:', err);
+  db.Game.findByIdAndDelete(req.params.id, (err, deletedGame) => {
+    if (err) {
+      console.log('Error in games#destroy:', err)
 
-        res.send("Incomplete games#destroy controller function");
-    });
+      res.send("Incomplete games#destroy controller function");
+    }
+
+    res.send("Incomplete games#destroy controller function");
+  });
 };
 
 
 module.exports = {
-    index,
-    show,
-    create,
-    update,
-    destroy,
+  index,
+  show,
+  create,
+  update,
+  destroy,
 };
