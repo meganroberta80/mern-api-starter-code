13c38a839c40dabb05ac6cd1dde4ab99c8da8c18
kbbushman <k.bushman@yahoo.com>
2021-06-08 20:20:02 -0700

    Updates filename

--- models/Game.js
+++ models/Game.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const GameSchema = new Schema({
-    title: String,
-    publisher: String,
-    coverArtUrl: String,
-    completed: Boolean,
+  title: String,
+  publisher: String,
+  coverArtUrl: String,
+  completed: Boolean,
 });
 
 const Game = mongoose.model('Game', GameSchema);
