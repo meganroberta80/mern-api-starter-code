f83c4e41f001c94d86c198f8cec2a132c409d1e7
kbbushman <k.bushman@yahoo.com>
2021-06-08 20:28:30 -0700

    Adds CORS todo

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const routes = require('./routes');
 const port = process.env.PORT || 4000;
 const app = express();
 
+// TODO: middleware - CORS
+
 // middleware - JSON parsing
 // app.use(express.json());
 
 // middleware - API routes
 app.use('/api/v1/games', routes.games);
 
-// connection
+// listen
 app.listen(port, () => console.log(`Server is running on port ${port}`));
